1b164806f8d49c09460de974227e43df532a8adc
Software Shinobi <troy@softwareshinobi.com>
2025-05-07 08:21:27 -0400

    automated terminal push

--- configure.bash
+++ configure.bash
@@ -1,7 +1,20 @@
+#!/bin/bash
 
 ##
 
-MAIL_LOC=/tmp/volume/intranet/email/
+reset
+
+clear
+
+##
+
+set -e
+
+set -x
+
+##
+
+MAIL_LOC=/volumes/intranet/email
 
 ##
 
@@ -11,6 +24,6 @@ sudo chown "$USER":"$USER" $MAIL_LOC -R
 
 ##
 
-docker run --rm -e MAIL_USER=wordpress@shinobinet.online -e MAIL_PASS=embanet -it mailserver/docker-mailserver /bin/sh -c 'echo "$MAIL_USER|$(doveadm pw -s SHA512-CRYPT -u $MAIL_USER -p $MAIL_PASS)"' >> $MAIL_LOC/config/postfix-accounts.cf
+docker run --rm -e MAIL_USER=shinobi@embanet.online -e MAIL_PASS=embanet -it mailserver/docker-mailserver /bin/sh -c 'echo "$MAIL_USER|$(doveadm pw -s SHA512-CRYPT -u $MAIL_USER -p $MAIL_PASS)"' >> $MAIL_LOC/config/postfix-accounts.cf
 
-docker run --rm -e MAIL_USER=shinobi@shinobinet.online -e MAIL_PASS=embanet -it mailserver/docker-mailserver /bin/sh -c 'echo "$MAIL_USER|$(doveadm pw -s SHA512-CRYPT -u $MAIL_USER -p $MAIL_PASS)"' >> $MAIL_LOC/config/postfix-accounts.cf
+docker run --rm -e MAIL_USER=services@embanet.online -e MAIL_PASS=embanet -it mailserver/docker-mailserver /bin/sh -c 'echo "$MAIL_USER|$(doveadm pw -s SHA512-CRYPT -u $MAIL_USER -p $MAIL_PASS)"' >> $MAIL_LOC/config/postfix-accounts.cf
